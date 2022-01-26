ddef9cecc37308a997b239c8269b123107cdc1a0
rahulsinghkk <35390632+rahulsinghkk@users.noreply.github.com>
2022-01-26 18:28:54 +0530

    Added DOcker

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -3,6 +3,7 @@ const express = require('express')
 const launchesRouter = require("./launches/launches.router");
 const plantesRouter = require("./planets/planets.router");
 
+
 const api = express.Router();
 
 api.use('/planets',plantesRouter);
